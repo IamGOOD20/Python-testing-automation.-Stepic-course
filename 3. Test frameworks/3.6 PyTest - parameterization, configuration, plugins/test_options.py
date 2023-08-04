f4e1eca8bc5a125b2a0bd51caee68bdadb191494
IamGOOD <rukshenas1990@gmail.com>
2023-08-04 19:05:58 -0400

    PyTest parameterization, configuration and plagins

---
+++ 3. Test frameworks/3.6 PyTest - parameterization, configuration, plugins/test_options.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
--- 3. Test frameworks/conftest.py
+++ 3. Test frameworks/conftest.py
@@ -6,9 +6,31 @@
 # @pytest.fixture(scope='function')
 # def browser():
 #     print('\nStart browser for test')
-#     browser = webdriver.Chrome()
+#     browser = webdriver.Firefox()
 #     yield browser
 #     print('\nbrowser quit')
 #     browser.quit()
-#
-#
+
+
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None, help='Choose browser: chrome or firefox')
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("CONFTEST LOADED")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
