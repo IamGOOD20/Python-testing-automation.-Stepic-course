a24e62904aa9f578ca4bdd8c28c8a2b5d8c16c43
IamGOOD <rukshenas1990@gmail.com>
2023-08-08 11:06:55 -0400

    3.6. PyTest - parameterization, configuration, plugins. Final project is finished

--- 3. Test frameworks/3.6 PyTest - parameterization, configuration, plugins/final project/conftest.py
+++ 3. Test frameworks/3.6 PyTest - parameterization, configuration, plugins/final project/conftest.py
@@ -1,28 +1,24 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
 def pytest_addoption(parser):
-    parser.addoption('--language', help='Choose browser language')# language=None
+    parser.addoption('--language', action='store', help='language of browser can be: [ru, en, fr, es ...]')
 
-
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='class')
 def browser(request):
     print('\nfinal project start browser')
     browser = webdriver.Chrome()
     language = request.config.getoption('language')
-
-    # language = None
-    if language == 'ru':
-        pass
-    elif language == 'en':
-        pass
-    elif language =='es':
-        pass
+    if language:
+        browser.get(link)
+        Select(browser.find_element(By.TAG_NAME, 'select')).select_by_value(language)
+        browser.find_element(By.XPATH, '//button[@class="btn btn-default"]').click()
     else:
-        raise pytest.UsageError('--language must be "ru" or "en" or "es"')
-
-
-
+        raise pytest.UsageError('parameter not specified --language="language"')
     yield browser
     print('\nfinal project quit browser')
     browser.quit()
